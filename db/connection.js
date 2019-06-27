5db923ad6536e38e71bb04ea3300bf5813cb2018
Brent Walker <brentwalker@Brents-MacBook-Pro.local>
2019-06-26 21:04:52 -0400

    fixed db

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,4 @@
-// // requirements: import mongoose
-// const mongoose = require('mongoose')
 
-
-// // Connect to a local database called "fleet-builder"
-// mongoose.connect("mongodb://localhost/fleet-builder", {useNewUrlParser:true}).then(()=>{
-//     console.log ("Connected to MongoDB boy")
-// })
-// // When it connects, then console.log "Connected to MongoDB"
-
-
-// // export your mongoose connection
-// module.exports = mongoose
 
 const mongoose = require('mongoose');
 
